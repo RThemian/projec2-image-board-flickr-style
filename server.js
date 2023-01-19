b8abaa582b2c6851c46d7a1b397b76c41566dfde
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2023-01-18 17:14:25 -0700

    wip: adds set up mongoose db

---
+++ server.js
@@ -0,0 +1,31 @@
+//require dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+
+// create express app
+const app = express();
+
+
+
+// configure settings
+//this must come before any process.env variables are used
+require('dotenv').config();
+
+
+//connect to database
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.DATABASE_URL);
+
+
+const db = mongoose.connection;
+
+db.on('error', (err) => console.error(err.message + ' is Mongo not running?'));
+db.on('connected', () => console.log('mongoDB connected: '));
+
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})

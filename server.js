4fb8328b0ec958c928f944e85121cab52f2726b0
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2023-01-21 10:32:20 -0700

    added Heroku

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 //require dependencies
+
 const express = require('express');
 const mongoose = require('mongoose');
 
-const Book = require("./models/book.js");
+const booksRouter = require('./controllers/books.js');
+const methodOverride = require("method-override");
 
 // create express app
 const app = express();
-
 
 
 // configure settings
@@ -28,74 +29,9 @@ db.on('connected', () => console.log('mongoDB connected: '));
 // mount middleware
 //grab form data and add it to req.body
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-// seed route
-const data = require('./data');
-
-app.get('/books/seed', (req, res) => {
-
-    Book.deleteMany({}, (err, results) => {
-        Book.create(data, (err, books) => {
-            res.redirect('/books');
-        })
-    })
-
-    
-})
-
-// INDUCES routes
-
-// index route
-// Index
-app.get("/", (req, res) =>{
-    res.redirect("/books");
-})
-
-
-app.get("/books", (req, res) => {
-    Book.find({}, (error, allBooks) => {
-      res.render("index.ejs", {
-        books: allBooks,
-      })
-    })  
-  })
-
-
-// new route
-app.get("/books/new", (req, res) => {
-    res.render("new.ejs")
-  })
-
-
-// delete route
-
-// update route
-
-// create route
-app.post('/books', (req,res) => {
-    if(req.body.completed === "on") {
-        req.body.completed = true;
-    } else {
-        req.body.completed = false;
-    }
-
-    Book.create(req.body, (err, createdBook) => {
-       res.redirect("/books")
-
-    })
-
-})
-
-
-// edit route
-
-// show route
-app.get("/books/:id", (req, res) => {
-    Book.findById(req.params.id, (err, foundBook) => {
-        res.render("show.ejs", {book: foundBook} )
-
-    })
-})
+app.use(booksRouter);
 
 
 
